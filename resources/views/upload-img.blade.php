91f649cf59764ca6b7fd52637779c54a82e836fa
Hiicup <hiicup@qq.com>
2019-09-21 21:11:43 +0800

    dev

--- resources/views/upload-img.blade.php
+++ resources/views/upload-img.blade.php
@@ -2,6 +2,7 @@
     <label class="layui-form-label">{{ $title }}</label>
     <div class="layui-input-block">
         <div class=" input-x">
+            <?php $layui->model($model); ?>
             {!! $layui->uploadImgInline($name,$tips,$placeholder) !!}
         </div>
     </div>
